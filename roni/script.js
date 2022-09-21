2ffc62c90601013be7da052ecf17d6d5caba917d
Yonatan Katz <katzyonatan@gmail.com>
2022-09-21 18:34:31 +0300

    roni

--- roni/script.js
+++ roni/script.js
@@ -1,42 +1,42 @@
 values = {
 	"red": {
 		backgroundColor: "#ffb3b3",
-		answer: "1357",
+		answer: "6523",
 		title: "הקבוצה האדומה"
 	},
 	"blue": {
 		backgroundColor: "#7af5ef",
-		answer: "4573",
+		answer: "5523",
 		title: "הקבוצה הכחולה"
 	},
 	"green": {
 		backgroundColor: "#7af5a1",
-		answer: "4779",
+		answer: "4523",
 		title: "הקבוצה הירוקה"
 	},
 	"gray": {
 		backgroundColor: "#918e90",
-		answer: "8354",
+		answer: "0523",
 		title: "הקבוצה האפורה"
 	},
 	"yellow": {
 		backgroundColor: "#e4fa23",
-		answer: "1982",
+		answer: "1523",
 		title: "הקבוצה הצהובה"
 	},
 	"pink": {
 		backgroundColor: "pink",
-		answer: "2544",
+		answer: "7523",
 		title: "הקבוצה הורודה"
 	},
 	"orange": {
 		backgroundColor: "orange",
-		answer: "2743",
+		answer: "7523",
 		title: "הקבוצה הכתומה"
 	},
 	"white": {
 		backgroundColor: "white",
-		answer: "8763",
+		answer: "9523",
 		title: "הקבוצה הלבנה"
 	},	
 };
